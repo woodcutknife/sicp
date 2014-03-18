6edfa78a514041312bfbd09bbc409bac73d4939d
zcwwzdjn <zcwwzdjn@hotmail.com>
2014-03-18 20:14:57 +0800

    rewrite 2.41, more elegant :)

--- 2.41/sum-eq-s-triples.scm
+++ 2.41/sum-eq-s-triples.scm
@@ -1,20 +1,9 @@
-(define (sum-eq-s-triples n s)
-  (map (lambda (pair) 
-         (append pair (list (- s (car pair) (cadr pair))))) ; generate triple from pair
-       (filter (lambda (pair)
-                 (let ((i (car pair))
-                       (j (cadr pair)))
-                   (let ((k (- s i j)))
-                     (and (not (= k i))
-                          (not (= k j))
-                          (not (= i j))
-                          (> k 0)
-                          (< k (+ n 1)))))) ; let k = s - i - j, then check if triple (i, j, k) is valid
-               (flatmap (lambda (i)
-                          (map (lambda (j)
-                                 (list i j))
-                               (gen-range 1 n)))
-                        (gen-range 1 n))))) ; generate {(i, j) : 1 <= i <= n, 1 <= j <= n}
+(define (gen-range l r)
+  (if (> l r)
+    '()
+    (cons l
+          (gen-range (+ l 1)
+                     r))))
 
 (define (flatmap proc seq)
   (accumulate append '() (map proc seq)))
@@ -25,7 +14,27 @@
     (op (car seq)
         (accumulate op init (cdr seq)))))
 
-(define (gen-range l r)
-  (if (> l r)
-    '()
-    (cons l (gen-range (+ l 1) r))))
+(define (for . lists) ; if there are n lists, procedure `for` will return a list of n-tuples, iterating i-th dimension with items in i-th list
+  (define (my-for lists)
+    (if (null? lists)
+      (list (list))
+      (flatmap (lambda (i)
+                 (map (lambda (l)
+                        (cons i l))
+                      (my-for (cdr lists))))
+               (car lists))))
+  (my-for lists))
+
+(define (sum-eq-s-triples n s)
+  (filter (lambda (triple)
+            (let ((i (car triple))
+                  (j (cadr triple))
+                  (k (caddr triple)))
+              (and (not (= i j))
+                   (not (= j k))
+                   (not (= k i))
+                   (= s
+                      (+ i j k)))))
+          (for (gen-range 1 n)
+               (gen-range 1 n)
+               (gen-range 1 n))))
